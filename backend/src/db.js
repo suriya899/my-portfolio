fd9e54e21ad54afc75f680aed09af9d885dfc7a4
Suriya <ktaro112@gmail.com>
2026-05-28 13:42:38 +0700

    fix: add SSL support for Neon PostgreSQL

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,12 +7,18 @@ import pg from "pg";
 const { Pool } = pg;
 
 // ดึงค่า config จาก .env ที่ตั้งไว้
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+//เชื่อม neon
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // ← Neon ต้องการบรรทัดนี้!
 });
 
 export default pool;
